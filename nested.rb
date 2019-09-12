552b076e4dd9699e4b5b938b1bfebf1d764924d2
steven nguyen <sqnguyen829@gmail.com>
2019-09-12 03:52:34 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -79,7 +79,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-    programmer_hash[:yukihiro_matsumoto]
+    programmer_hash[:yukihiro_matsumoto]= {}
     programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
     programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
     programmer_hash
