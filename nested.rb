1232fe3c3592e4a323f868d95837e3ae720e891f
steven nguyen <sqnguyen829@gmail.com>
2019-09-12 03:58:04 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -124,7 +124,8 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-programmer_hash[:dennis_ritchie][:languages].push("")
+programmer_hash[:dennis_ritchie][:languages].push("Assembly")
+programmer_hash
 end
 
 
